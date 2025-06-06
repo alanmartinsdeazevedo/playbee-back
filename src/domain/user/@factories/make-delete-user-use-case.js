faa2b4c512e15a014c37b7dfd6052e664fb74201
Alan Martins <alan.azevedo@alaresinternet.com.br>
2025-06-06 02:15:24 -0300

    fix

--- src/domain/user/@factories/make-delete-user-use-case.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeDeleteUserUseCase = makeDeleteUserUseCase;
-const prisma_user_repository_1 = require("@/domain/user/repositories/prisma/prisma-user-repository");
-const delete_user_use_case_1 = require("../use-cases/delete-user-use-case");
-function makeDeleteUserUseCase() {
-    const userRepository = new prisma_user_repository_1.PrismaUserRepository();
-    return new delete_user_use_case_1.DeleteUserUseCase(userRepository);
-}
--- src/domain/user/@factories/make-getall-user-use-case.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeGetAllUsersUseCase = makeGetAllUsersUseCase;
-const prisma_user_repository_1 = require("@/domain/user/repositories/prisma/prisma-user-repository");
-const getall_user_use_case_1 = require("@/domain/user/use-cases/getall-user-use-case");
-function makeGetAllUsersUseCase() {
-    const userRepository = new prisma_user_repository_1.PrismaUserRepository();
-    return new getall_user_use_case_1.GetAllUsersUseCase(userRepository);
-}
