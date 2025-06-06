57bd40908ddce7a2a3e8d0d0040d1b818192a325
Alan Martins <alan.azevedo@alaresinternet.com.br>
2025-06-06 02:31:12 -0300

    'fix'

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen({
-    port: 3003
-}).then(() => {
-    console.log('🚀 HTTP Server Runing!');
-});
